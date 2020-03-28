5f3098f65fdf0d7629775511216cfbb1ae4dae1e
nawras-haloui <nawhaloui@gmail.com>
2020-03-28 17:07:36 +0100

    clash free time

---
+++ projectttt/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AngularMaterialModule } from './material.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselComponent } from './carousel/carousel.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NewComponentComponent } from './new-component/new-component.component';
+import { ChatComponent } from './chat/chat.component';
+import { UniversitiesComponent } from './universities/universities.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarouselComponent,
+    NewComponentComponent,
+    ChatComponent,
+    UniversitiesComponent,
+    SubscribeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule { }
